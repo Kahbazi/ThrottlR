08ab1af6f8c47cb67389fb4ddaf32b7963d4af25
Kahbazi <A.Kahbazi@gmail.com>
2020-07-13 00:34:00 +0430

    Fix build

--- test/ThrottlR.Tests/RateLimitProcessorTests.cs
+++ test/ThrottlR.Tests/RateLimitProcessorTests.cs
@@ -10,14 +10,11 @@ namespace ThrottlR.Tests
 {
     public class RateLimitProcessorTests
     {
-        
-
         private static (ThrottlerService processor, TimeMachine timeMachine) CreateProcessor()
         {
-            var options = Options.Create(new ThrottleOptions());
             var store = new TestRateLimitStore();
             var clock = new TimeMachine();
-            return (new ThrottlerService(options, store, clock), clock);
+            return (new ThrottlerService(store, clock), clock);
         }
     }
 }
