67b7642e988b73b04fba5f1ca67085155fbfa16f
m.shakiba <work.shakiba@gmail.com>
2020-11-29 22:37:56 +0330

    changes required to comply with PR suggestions in https://github.com/Kahbazi/ThrottlR/pull/2

--- src/ThrottlR/Service/Store/DistributedCacheCounterStore.cs
+++ src/ThrottlR/Service/Store/DistributedCacheCounterStore.cs
@@ -25,13 +25,13 @@ namespace ThrottlR
                 options.SetAbsoluteExpiration(expirationTime.Value);
             }
 
-            var key = throttlerItem.GenerateCounterKey();
-            await _cache.SetAsync(key, ToBinary(counter).ToArray(), options, cancellationToken);
+            var key = GenerateThrottlerItemKey(throttlerItem);
+            await _cache.SetAsync(key, ToBinary(counter), options, cancellationToken);
         }
 
         public async ValueTask<Counter?> GetAsync(ThrottlerItem throttlerItem, CancellationToken cancellationToken)
         {
-            var key = throttlerItem.GenerateCounterKey();
+            var key = GenerateThrottlerItemKey(throttlerItem);
             var stored = await _cache.GetAsync(key, cancellationToken);
 
             if (stored != null)
@@ -44,16 +44,23 @@ namespace ThrottlR
 
         public async ValueTask RemoveAsync(ThrottlerItem throttlerItem, CancellationToken cancellationToken)
         {
-            var key = throttlerItem.GenerateCounterKey();
+            var key = GenerateThrottlerItemKey(throttlerItem);
             await _cache.RemoveAsync(key, cancellationToken);
         }
 
-        private static ReadOnlySpan<byte> ToBinary(Counter counter)
+        public virtual string GenerateThrottlerItemKey(ThrottlerItem throttlerItem)
+        {
+            return throttlerItem.GenerateCounterKey("Throttler");
+        }
+
+        private static byte[] ToBinary(Counter counter)
         {
 
             var data = new byte[I32Len + I64Len];
-            Buffer.BlockCopy(BitConverter.GetBytes(counter.Count), default, data, default, I32Len);
-            Buffer.BlockCopy(BitConverter.GetBytes(counter.Timestamp.Ticks), default, data, I32Len, I64Len);
+
+            // no need to check for success, it only fails on size mismatch which shouldn't happen
+            BitConverter.TryWriteBytes(data.AsSpan(default, I32Len), counter.Count);
+            BitConverter.TryWriteBytes(data.AsSpan(I32Len, I64Len), counter.Timestamp.Ticks);
             return data;
         }
 
